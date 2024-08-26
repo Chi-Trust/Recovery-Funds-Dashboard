23f188829ff99c12c8d8e8fcc3e98e260ca95e72
Rob Pitingolo <rpitingolo@urban.org>
2024-08-26 13:32:32 -0400

    Re-render

--- render.R
+++ render.R
@@ -1,6 +1,28 @@
 library(rmarkdown)
 library(purrr)
 
+# Load Merriweather font
+uname <- "RPitingo"
+#uname <- "astern"
+
+pc_font_path <- paste0("C:/Users/", uname, "/AppData/Local/Microsoft/Windows/Fonts/")
+mac_font_path <- "~/Library/Fonts"
+font_path <-
+  switch(Sys.info()[["sysname"]],
+         Windows = {
+           pc_font_path
+         },
+         Darwin = {
+           "~/Library/Fonts"
+         }
+  )
+
+font_import(
+  paths = font_path,
+  pattern = "Merriweather",
+  prompt = FALSE
+)
+
 site_pages <- c(
   "infrastructure-funds.Rmd",
   "recovery-funds.Rmd",
