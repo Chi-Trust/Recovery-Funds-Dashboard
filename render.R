b20a7631db9988bc3b0dd2b55952115fe686feab
Rob Pitingolo <rpitingolo@urban.org>
2024-08-26 14:12:04 -0400

    Fix logo size

--- render.R
+++ render.R
@@ -1,27 +1,6 @@
 library(rmarkdown)
 library(purrr)
-
-# Load Merriweather font
-uname <- "RPitingo"
-#uname <- "astern"
-
-pc_font_path <- paste0("C:/Users/", uname, "/AppData/Local/Microsoft/Windows/Fonts/")
-mac_font_path <- "~/Library/Fonts"
-font_path <-
-  switch(Sys.info()[["sysname"]],
-         Windows = {
-           pc_font_path
-         },
-         Darwin = {
-           "~/Library/Fonts"
-         }
-  )
-
-font_import(
-  paths = font_path,
-  pattern = "Merriweather",
-  prompt = FALSE
-)
+library(extrafont)
 
 site_pages <- c(
   "infrastructure-funds.Rmd",
